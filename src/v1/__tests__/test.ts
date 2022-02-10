edd7ecdec4341d656bfcccf73a5e568c7a1dfbdf
Sho Shimauchi <sho.shimauchi@open-c.jp>
2022-02-10 13:03:34 +0900

    fix #49: install jest-mock

--- src/v1/__tests__/test.ts
+++ src/v1/__tests__/test.ts
@@ -1,4 +1,4 @@
-import { mocked } from 'ts-jest/utils';
+import { mocked } from 'jest-mock';
 import { KENALL, NTACorporateInfoSearchMode } from '..';
 import { AxiosInstance } from 'axios';
 import axios from 'axios';
